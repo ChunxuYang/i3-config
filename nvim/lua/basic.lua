dedb8b078c7c19fee12249719fe3c3564e2a215f
Chunxu Yang <chunxuyang@ucla.edu>
2023-02-01 21:51:39 -0800

    feat: initial i3 config

---
+++ nvim/lua/basic.lua
@@ -0,0 +1,33 @@
+-- utf8 support
+vim.opt.encoding = "UTF-8"
+vim.opt.fileencoding = "utf-8"
+
+-- line number
+vim.opt.number = true
+vim.opt.cursorline = true
+
+-- scroll
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+-- tabstop
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.wrap = false
+vim.opt.shiftwidth = 2
+vim.opt.showtabline = 2
+
+-- search options
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+
+-- mouse
+vim.opt.mouse = "a"
+
+-- backup
+vim.opt.backup = true
+
+-- completion
+updatetime = 300
+
---
+++ polybar/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+killall -q polybar
+
+polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+
+
